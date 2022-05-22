adbc35231ed153903367d5a943cb46b1161f48d6
Aleš Dajčar <dajcara.04@spst.cz>
2022-05-22 21:45:48 +0200

    Přidejte soubory projektu.

---
+++ DuPraxe/Form1.cs
@@ -0,0 +1,46 @@
+﻿using ListLibrary;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace DuPraxe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        List<int> listCisel = new List<int>();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n = int.Parse(textBox1.Text);
+
+            
+
+            Operace.Vygenerovani(listCisel, n);
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Operace.Vypsani(listCisel, listBox1);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int pocet;
+            pocet = Operace.pocetJedinecnych(listCisel);
+            MessageBox.Show("Pocet vygenerovaných jedinecnych čísel je " + pocet);
+        }
+    }
+}
---
+++ ListLibrary/Operace.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace ListLibrary
+{
+    public class Operace
+    {
+        public static void Vygenerovani(List<int> list, int n)
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < n; ++i)
+            {
+                int a = rnd.Next(-100, 100);
+                list.Add(a);
+            }
+           
+        }
+
+        public static void Vypsani(List<int> list,ListBox l)
+        {
+            l.Items.Clear();
+
+            foreach (int cislo in list)
+            {
+                l.Items.Add(cislo);
+            }
+        }
+
+        public static int pocetJedinecnych(List<int> list)
+        {
+            List<int> pocetJedinec = list.Distinct().ToList();
+            int pocet = pocetJedinec.Count;
+            return pocet;
+        }
+        
+    }
+}
